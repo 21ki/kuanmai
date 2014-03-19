27cc1f97eed2387f9d81c736cb72de3efb4f6de0
Bobom <lingbo_wx@hotmail.com>
2014-03-19 20:41:15 +0800

    d

--- kmjxc/bl/SupplierManager.cs
+++ kmjxc/bl/SupplierManager.cs
@@ -18,9 +18,70 @@ namespace KM.JXC.BL
 
         }
 
+        public SupplierManager(User user)
+            : base(user)
+        {
+        }
+
+        public List<Supplier> GetSupplies(int shop_id,int pageIndex,int pageSize,out int totalRecord)
+        {
+            List<Supplier> supplies = new List<Supplier>();
+            totalRecord = 0;
+            using (KuanMaiEntities db = new KuanMaiEntities())
+            {
+                totalRecord = db.Supplier.Count();
+            }
+
+            return supplies;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="supplier_id"></param>
+        /// <returns></returns>
+        public Supplier GetSupplierDetail(int supplier_id)
+        {
+            Supplier supplier = null;
+
+            using (KuanMaiEntities db = new KuanMaiEntities())
+            {
+                var s = from sp in db.Supplier where sp.Supplier_ID == supplier_id select sp;
+
+                if(s!=null && s.ToList<Supplier>().Count>0)
+                {
+                    supplier=s.ToList<Supplier>()[0];
+                }
+            }
+
+            return supplier;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="supplier"></param>
+        /// <returns></returns>
         public bool CreateSupplier(Supplier supplier)
         {
             bool result = false;
+            
+            if (string.IsNullOrEmpty(supplier.Name))
+            {
+                throw new KMJXCException("供应商名称不能为空");
+            }
+
+            if (supplier.User_ID == 0 && this.CurrentUser!=null)
+            {
+                supplier.User_ID = this.CurrentUser.User_ID;
+            }
+
+            using (KuanMaiEntities db = new KuanMaiEntities())
+            {
+                db.Supplier.Add(supplier);
+                db.SaveChanges();
+                result = true;
+            }
 
             return result;
         }
