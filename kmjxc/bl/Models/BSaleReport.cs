bb0256673b46d05ae22bc36475fc01cb8010f6c2
Danny <lingbo_wx@hotmail.com>
2014-06-13 17:00:56 +0800

    ddd

---
+++ kmjxc/bl/Models/BSaleReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KM.JXC.BL.Models
+{
+    public class BSaleReport
+    {
+        public long Product_ID { get; set; }
+        public long Parent_Product_ID { get; set; }
+    }
+}
