bbbc8b8407ee27af80fce57ce1303835bda323c2
Bobom <lingbo_wx@hotmail.com>
2014-06-30 20:56:28 +0800

    ddd

---
+++ kmjxc/bean/Util/Encrypt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+namespace KM.JXC.Common.Util
+{
+    public class Encrypt
+    {
+        public static string MD5(string resource)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            string a = BitConverter.ToString(md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(resource)));
+            a = a.Replace("-", "");
+            return a;
+        }
+    }
+}
---
+++ kmjxc/bl/Admin/SystemAdmin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KM.JXC.Common.Util;
+using KM.JXC.BL.Admin;
+using KM.JXC.DBA;
+using KM.JXC.Common.KMException;
+
+namespace KM.JXC.BL.Admin
+{
+    public class SystemAdmin
+    {
+        public void Login(int user_id,string password)
+        {
+            using (KuanMaiEntities db = new KuanMaiEntities())
+            {
+                User user=(from u in db.User where u.IsSystemUser==true && u.User_ID==user_id select u).FirstOrDefault<User>();
+                if (user == null)
+                {
+                    throw new KMJXCException("用户名不存在");
+                }
+
+                string md5Password = Encrypt.MD5(password);
+
+                if (md5Password != user.Password)
+                {
+                    throw new KMJXCException("密码不正确");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="info"></param>
+        public void SetCorpInfo(Corp_Info info)
+        {
+
+        }
+    }
+}
