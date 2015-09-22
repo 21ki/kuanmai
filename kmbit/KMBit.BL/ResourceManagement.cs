5cbd6c1a04c8e4dd7702b20218e01c143327bfb5
Danny <lingbo_wx@hotmail.com>
2015-09-22 10:15:02 +0800

    Signed-off-by: Danny <lingbo_wx@hotmail.com>

--- kmbit/KMBit.BL/ResourceManagement.cs
+++ kmbit/KMBit.BL/ResourceManagement.cs
@@ -39,7 +39,7 @@ namespace KMBit.BL
                 logger.Error("resource name cannot be empty");
                 throw new KMBitException("资源名称不能为空");
             }
-
+            resource.Enabled = true;
             using (chargebitEntities db = new chargebitEntities())
             {
                 db.Resource.Add(resource);
@@ -85,12 +85,42 @@ namespace KMBit.BL
         public bool CreateResourceTaocan(Resource_taocan taocan)
         {
             bool ret = false;
-            if(CurrentLoginUser.Permission.CREATE_RESOURCE_TAOCAN==0)
+            if (CurrentLoginUser.Permission.CREATE_RESOURCE_TAOCAN == 0)
             {
                 throw new KMBitException("没有权限创建资源套餐");
             }
-          
 
+            if (taocan.Quantity <= 0)
+            {
+                throw new KMBitException("套餐容量不能为零");
+            }
+
+            taocan.Enabled = true;
+            taocan.Created_time = DateTimeUtil.ConvertDateTimeToInt(DateTime.Now);
+            taocan.Updated_time = taocan.Created_time;
+            using (chargebitEntities db = new chargebitEntities())
+            {
+                Taocan ntaocan = (from t in db.Taocan where t.Sp_id == taocan.Sp_id && t.Quantity == taocan.Quantity select t).FirstOrDefault<Taocan>();
+                Sp sp = (from s in db.Sp where s.Id == taocan.Sp_id select s).FirstOrDefault<Sp>();
+                if (ntaocan == null)
+                {
+                    string taocanName = sp != null ? sp.Name + " " + taocan.Quantity.ToString() + "M" : "全网 " + taocan.Quantity.ToString() + "M";
+                    ntaocan = new Taocan() { Created_time = taocan.Created_time, Description = taocanName, Name = taocanName, Sp_id = taocan.Sp_id, Quantity = taocan.Quantity, Updated_time = taocan.Updated_time };
+                    db.Taocan.Add(ntaocan);
+                    db.SaveChanges();
+                }
+                if (ntaocan.Id > 0)
+                {
+                    db.Resource_taocan.Add(taocan);
+                    db.SaveChanges();
+                    ret = true;
+                }
+                else
+                {
+                    throw new KMBitException("套餐创建失败");
+                }
+
+            }
             return ret;
         }
     }
